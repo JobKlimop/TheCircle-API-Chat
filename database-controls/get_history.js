5edf58ec107a011e8ae9f93e9c529595660470a7
Mark <mamjong@avans.nl>
2018-06-18 10:20:15 +0200

    Limited returned history.

--- database-controls/get_history.js
+++ database-controls/get_history.js
@@ -2,7 +2,11 @@ const Chatroom = require('../models/chatroom');
 
 function getHistory(roomOwner) {
 	return new Promise((resolve, reject) => {
-		Chatroom.findById(roomOwner).populate('messages')
+		Chatroom.findById(roomOwner)
+			.populate({
+				path: 'messages',
+				options: { limit: 10 }
+			})
 			.then((retrievedChatroom) => {
 				resolve(retrievedChatroom.messages);
 			})
