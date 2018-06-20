27bbdcd589beb758f20a10eddcedbaa98995ede9
Koen Kamman <kammankoen@gmail.com>
2018-06-20 12:53:06 +0200

    fixed tests

--- test/event_test.js
+++ test/event_test.js
@@ -4,8 +4,36 @@ const io = require('socket.io-client');
 const assert = require('assert');
 
 let socket;
+const username = 'mika';
+const room = 'test-room-1';
+const userCert = '-----BEGIN CERTIFICATE-----\r\n' +
+	'MIIEfDCCAmSgAwIBAgIBATANBgkqhkiG9w0BAQUFADCBmTELMAkGA1UEBhMCTkwx\r\n' +
+	'FjAUBgNVBAgMDU5vb3JkLUJyYWJhbnQxDjAMBgNVBAcMBUJyZWRhMRMwEQYDVQQK\r\n' +
+	'DApUaGUgQ2lyY2xlMRMwEQYDVQQLDApUaGUgQ2lyY2xlMRMwEQYDVQQDDApUaGUg\r\n' +
+	'Q2lyY2xlMSMwIQYJKoZIhvcNAQkBFhR0aGVjaXJjbGVAdGhlLmNpcmNsZTAeFw0x\r\n' +
+	'ODA2MjAwOTQ3MzBaFw0xOTA2MjAwOTQ3MzBaMGkxDTALBgNVBAMTBG1pa2ExCzAJ\r\n' +
+	'BgNVBAYTAk5MMRYwFAYDVQQIEw1Ob29yZC1CcmFiYW50MQ4wDAYDVQQHEwVCcmVk\r\n' +
+	'YTETMBEGA1UEChMKVGhlIENpcmNsZTEOMAwGA1UECxMFVXNlcnMwggEiMA0GCSqG\r\n' +
+	'SIb3DQEBAQUAA4IBDwAwggEKAoIBAQCSKZltqDNxXh51aKAfTo5tNdkwVcuNtWKL\r\n' +
+	'9KI2zZLLzpdlgYslOKdTKqcbUmo/09L6wEhNj9li2RzaZi98dzKNa20g/x66zpA5\r\n' +
+	'7zBPs+E6Pho6egz1kMn98VHm36OqWZu5/Z/XdJN2ypB5T1dTIKuYcuFvKpoo19VV\r\n' +
+	'MEDunig+U42ajS1SdGRpbhQkI7x8ajELA3WpvHxYF6amYrPNZqZeu7MSMq375G/e\r\n' +
+	'kk9a7wKNcchgj9JtGDYkERo1en8UGWC3wWp6A5kpV8lyHNX2AJ+x9CKcAb4704cR\r\n' +
+	'1c3T83saL9qqbRhzmswiOyYuIhWQXfsn5ocN/IAHm7OW3JAvig0HAgMBAAEwDQYJ\r\n' +
+	'KoZIhvcNAQEFBQADggIBAEH+uX86YWCREwSvn0ruj2bExF7OIDkV+pHjexHqznPc\r\n' +
+	'yJ0AJD9xqqdpg3QbVvP9vwNC8IzDXqf2Siy/YkMJIFVKHRgOZf73W4Arz959mlP3\r\n' +
+	'64nxE1xc79v5Dq7Yj/GXAXMI3uWFwZlFp+hRD4QqQ4Hz9K/3OHZ5hW+24teYB71X\r\n' +
+	'mgNMBq0cB+xWqmQapr5VD2vQPg7pKWjt//nAhhfPbjKN+LusM6/PkFx9Hq6GR0dx\r\n' +
+	'VEj/7mglHlLnZf5hFJtPH6tvOQwyr/cQEw8M89ace/YlUk9WEGnmLQofoSyDeEuj\r\n' +
+	'zVTXbgCbg/sy1wTz+2VbVoJnleadL1/VauBUs5vGhphsR4E40NArqcU87vyVtjvM\r\n' +
+	'kDLsS+IBHIIYZeJ3qT33hWsLs6rsTZJ4vTTU3VqgIDajlqTi0ZI4zvGS8OEFtA+D\r\n' +
+	'/HueMWszdQ/LFoWEIJmTT/tJVe9rhuBjbOJyJLnTjfj13vWsUoXh7ibZMR6JajV+\r\n' +
+	'WoF/S+NQPaZ1pKw0QTDnaHRRZIqnh8qrVXOj+eEZk3z125OOpnRrc1RLWl+sqRhO\r\n' +
+	'JtUaRoJgFS+T93/EOC/5t8+XP6UwlV3VgoyVhNShS5KnohDLNw0AU31Y+QO/pQ3T\r\n' +
+	'8braAYT+a2VzsKyEKkWIyDFP4g7WYQ8X+sNigyaRavAaqjmhVcAt9GyRA7m94SCg\r\n' +
+	'-----END CERTIFICATE-----\r\n';
 
-xdescribe('Event Tests', () => {
+describe('Event Tests', () => {
 	beforeEach((done) => {
 		socket = io.connect('http://localhost:3000', {
 			'reconnection delay': 0,
@@ -15,7 +43,12 @@ xdescribe('Event Tests', () => {
 		});
 
 		socket.on('connect', () => {
-			done();
+			socket.on("verified", (verified) => {
+				if (verified) {
+					done();
+				}
+			});
+			socket.emit("verify_identity", {certificate: userCert});
 		});
 
 		socket.on('disconnect', () => {
@@ -27,17 +60,6 @@ xdescribe('Event Tests', () => {
 			socket.disconnect();
 		}
 		done();
-	});
-
-	const username = 'Test-User';
-	const room = 'test-room-1';
-
-	it('Client should receive a username_set event after setting a username', (done) => {
-		socket.emit('set_username', username);
-		socket.on('username_set', (setUsername) => {
-			assert(setUsername === username, "wrong user");
-			done();
-		});
 	});
 
 	it('Client should receive a room_joined event after joining a room', (done) => {
@@ -91,7 +113,6 @@ xdescribe('Event Tests', () => {
 			timestamp: timestamp
 		};
 
-		socket.emit('set_username', username);
 		socket.emit('join_room', room);
 		socket.emit('message', msg);
 		socket.on('message', (msg) => {
